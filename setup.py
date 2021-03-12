982f3919ef868e2f9ae9f130f9eba0ea4bccaa37
Zaki A <zaki@live.ca>
2021-03-12 14:03:15 -0600

    Add dependencies for reading NIfTI and MAT files

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
         name='src',
         version = '1.0.0', 
-        install_requires = ['pytest','numpy','scipy','lmfit'],
+        install_requires = ['pytest','numpy','scipy','lmfit','nibabel','mat73'],
         packages = find_packages()
         )
